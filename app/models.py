cae6f1edf1960142b4e6b7f1746e746d08799715
Mridhini Koyalkar <Mridhini.koyalkar@student.unsw.edu.au>
2024-08-29 22:08:13 +1000

    added Users and Projects

--- app/models.py
+++ app/models.py
@@ -6,16 +6,12 @@ class User:
         self.friends = []
         self.progress = []
 
-    def add_friend(self, friend_username):
-        if friend_username not in self.friends:
-            self.friends.append(friend_username)
-
-    def update_progress(self, project_id, completion_percentage):
-        for i, (proj_id, _) in enumerate(self.progress):
-            if proj_id == project_id:
-                self.progress[i] = (project_id, completion_percentage)
+    def update_progress(self, project_id, question_id, completion_percentage):
+        for i, (proj_id, ques_id, _) in enumerate(self.progress):
+            if proj_id == project_id and ques_id == question_id:
+                self.progress[i] = (project_id, question_id, completion_percentage)
                 return
-        self.progress.append((project_id, completion_percentage))
+        self.progress.append((project_id, question_id, completion_percentage))
 
     def to_dict(self):
         return {
@@ -25,6 +21,10 @@ class User:
             'friends': self.friends,
             'progress': self.progress
         }
+    
+    def add_friend(self, friend_username):
+        if friend_username not in self.friends:
+            self.friends.append(friend_username)
 
     @staticmethod
     def from_dict(data):
@@ -33,3 +33,29 @@ class User:
         user.friends = data.get('friends', [])
         user.progress = data.get('progress', [])
         return user
+
+class Project:
+    def __init__(self, project_id, project_name):
+        self.project_id = project_id
+        self.project_name = project_name
+        self.questions = {}  # {question_id: points}
+
+    def add_question(self, question_id, points):
+        if question_id not in self.questions:
+            self.questions[question_id] = points
+
+    def get_question_points(self, question_id):
+        return self.questions.get(question_id, 0)
+
+    def to_dict(self):
+        return {
+            'project_id': self.project_id,
+            'project_name': self.project_name,
+            'questions': self.questions
+        }
+
+    @staticmethod
+    def from_dict(data):
+        project = Project(data['project_id'], data['project_name'])
+        project.questions = data.get('questions', {})
+        return project
