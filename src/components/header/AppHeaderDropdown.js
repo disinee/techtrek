64d1f492a79a89da5cad0dc02e6bfe3aa5f35c78
Disinee Kumanayaka <z5359105@ad.unsw.edu.au>
2024-08-29 21:46:12 +1000

    added buttons and refactored the menu

--- src/components/header/AppHeaderDropdown.js
+++ src/components/header/AppHeaderDropdown.js
@@ -69,24 +69,10 @@ const AppHeaderDropdown = () => {
           <CIcon icon={cilSettings} className="me-2" />
           Settings
         </CDropdownItem>
-        <CDropdownItem href="#">
-          <CIcon icon={cilCreditCard} className="me-2" />
-          Payments
-          <CBadge color="secondary" className="ms-2">
-            42
-          </CBadge>
-        </CDropdownItem>
-        <CDropdownItem href="#">
-          <CIcon icon={cilFile} className="me-2" />
-          Projects
-          <CBadge color="primary" className="ms-2">
-            42
-          </CBadge>
-        </CDropdownItem>
         <CDropdownDivider />
-        <CDropdownItem href="#">
+        <CDropdownItem href="#login">
           <CIcon icon={cilLockLocked} className="me-2" />
-          Lock Account
+          Log Out 
         </CDropdownItem>
       </CDropdownMenu>
     </CDropdown>
